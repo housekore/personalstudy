6082f3b58b47c7d794cadce2123571c289346010
José Jardel Luciano Ferreira da Silva <32444652+housekore@users.noreply.github.com>
2020-06-24 13:07:23 -0300

    Create listaOrdenada.py

---
+++ listaOrdenada.py
@@ -0,0 +1,19 @@
+#Lista ordenada
+minha_lista1 = [125,398,450,365,378,393,984,1,2,3,6,12,15]
+minha_lista2 = ["Abacate", "Zinco", "Pipoca", "Guará", "Açaí", "Quentão", "Dinheiro"]
+
+#Ordenar os numeros
+minha_lista1.sort()
+print(minha_lista1)
+
+#sort serve para ordenar alfabeticamente
+minha_lista2.sort()
+print(minha_lista2)
+
+#Ordenar decrescente
+minha_lista1.sort(reverse=True)
+print(minha_lista1)
+
+#Invertar a lista
+minha_lista1.reverse()
+print(minha_lista1)
