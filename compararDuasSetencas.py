91d8f4a05596ad655b247c64378beba4889dcda0
José Jardel Luciano Ferreira da Silva <32444652+housekore@users.noreply.github.com>
2020-06-24 22:27:22 -0300

    Create compararDuasSetencas.py

---
+++ compararDuasSetencas.py
@@ -0,0 +1,10 @@
+#Escreva um programa que compare se duas sequências digitadas pelo usuário são iguais.   
+
+seq1 = input("Por favor, informe a primeira sequência para comparação: ")
+seq2 = input("Por favor, informe a segunda sequência para comparação: ")
+
+if seq1 == seq2:
+	print("As sequências digitadas são iguais!")
+else:
+	print("As sequências digitadas são diferentes!")
+
